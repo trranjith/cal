a0becdbb3e3cfc44ca7fa40aeaf377e21711a72c
kartik <training_b3a.00.35@BDC3-DX-519QSYN.dir.svc.accenture.com>
2018-03-28 16:01:24 +0530

    Initial project creation commit

---
+++ src/main/java/org/cal/CalculatorDemo.java
@@ -0,0 +1,8 @@
+package org.cal;
+
+public class CalculatorDemo {
+	public static void main(String[] args) {
+		System.out.println("Sum of two numbers 10 and 20 is "+(new Calculator()).doAdd(10, 20));
+	}
+
+}
